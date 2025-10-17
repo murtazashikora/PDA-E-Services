4fb257202ea26f647e034e9efa7f7f8d23cf82db
Murtaza Shikora <124716374+murtazashikora@users.noreply.github.com>
2025-10-17 13:01:12 +0530

    Update SetLockScreenImage_Diwali.ps1

--- SetLockScreenImage_Diwali.ps1
+++ SetLockScreenImage_Diwali.ps1
@@ -1,4 +1,4 @@
-Start-BitsTransfer -Source 'https://github.com/murtazashikora/PDA-E-Services/blob/Wallpapers/PDA%20Pune.jpg?raw=true' -Destination c:\PNQ_Lockscreen.jpg
+Start-BitsTransfer -Source 'https://github.com/murtazashikora/PDA-E-Services/blob/PNQ-LockScreen/PNQ%20Diwali%202025.jpg?raw=true' -Destination c:\PNQ_Lockscreen.jpg
 
 REG DELETE HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /f
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP
@@ -6,3 +6,4 @@ REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v Loc
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v LockScreenImageUrl /t REG_SZ /d "c:\PNQ_Lockscreen.jpg" /f
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v LockScreenImageStatus /t REG_DWORD /d 1 /f
 icacls "C:\ProgramData\Microsoft\Windows\SystemData" /reset /t /c /l
+
