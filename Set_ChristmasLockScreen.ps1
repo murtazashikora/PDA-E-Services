d77dc2865ee52c0bce4bad15e2f9bedf053faf91
Murtaza Shikora <124716374+murtazashikora@users.noreply.github.com>
2025-12-24 20:57:46 +0530

    Create Set_ChristmasLockScreen.ps1

---
+++ Set_ChristmasLockScreen.ps1
@@ -0,0 +1,8 @@
+Start-BitsTransfer -Source 'https://github.com/murtazashikora/PDA-E-Services/blob/PNQ-LockScreen/PNQ%20Diwali%202025.jpg?raw=true' -Destination c:\PNQ_Lockscreen.jpg
+
+REG DELETE HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v LockScreenImagePath /t REG_SZ /d "c:\PNQ_Lockscreen.jpg" /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v LockScreenImageUrl /t REG_SZ /d "c:\PNQ_Lockscreen.jpg" /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v LockScreenImageStatus /t REG_DWORD /d 1 /f
+icacls "C:\ProgramData\Microsoft\Windows\SystemData" /reset /t /c /l
